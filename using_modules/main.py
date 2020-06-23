42e01c71b97616745ac43c972f93b6800993d299
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-23 16:50:26 +0800

    Creating and Using Python Modules

--- using_modules/main.py
+++ using_modules/main.py
@@ -1,6 +1,6 @@
 # python -m using_modules.main
-from using_modules import helpers
+from using_modules.helpers import extract_lower as e_low, extract_upper as e_up
 
 name = "Keith Thompson"
-print(f"Lowercase letters: {helpers.extract_lower(name)}")
-print(f"Uppercase letters: {helpers.extract_upper(name)}")
+print(f"Lowercase letters: {e_low(name)}")
+print(f"Uppercase letters: {e_up(name)}")
