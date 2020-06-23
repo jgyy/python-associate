827ba19d4bff66c3811ef6a003b768da618bfb4e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-23 22:04:19 +0800

    Creating and Using Python Packages

--- using_modules/main.py
+++ using_modules/main.py
@@ -1,6 +1,7 @@
 # python -m using_modules.main
-from using_modules.helpers import extract_upper, extract_lower
-import using_modules.extras as extras
+from using_modules.helpers.strings import extract_lower, extract_upper
+from using_modules.helpers import variables
 
-print(f"Lowercase letters: {extract_lower(extras.name)}")
-print(f"Uppercase letters: {extract_upper(extras.name)}")
+print(f"Lowercase letters (from strings): {extract_lower(variables.name)}")
+print(f"Uppercase letters (from package): {extract_upper(variables.name)}")
+print(f"Off of helpers: {extract_lower(variables.name)}")
