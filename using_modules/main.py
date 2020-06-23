959fe355ca01236413dbe189dd00519de2df1064
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-23 22:21:25 +0800

    Distributing and Installing Packages

--- using_modules/main.py
+++ using_modules/main.py
@@ -1,6 +1,6 @@
 # python -m using_modules.main
-from using_modules.helpers.strings import extract_lower, extract_upper
-from using_modules.helpers import variables
+from helpers.strings import extract_lower, extract_upper
+from helpers import variables
 
 print(f"Lowercase letters (from strings): {extract_lower(variables.name)}")
 print(f"Uppercase letters (from package): {extract_upper(variables.name)}")
