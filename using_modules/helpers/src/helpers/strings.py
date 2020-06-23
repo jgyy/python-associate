f8134d46200ef4d0875822dca817c39dec1bdfe9
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-23 22:27:33 +0800

    Docstrings, Doctests, and Shebangs

--- using_modules/helpers/src/helpers/strings.py
+++ using_modules/helpers/src/helpers/strings.py
@@ -1,6 +1,12 @@
-__all__ = ["extract_upper"]
-
+#  python -m doctest -v .\using_modules\helpers\src\helpers\strings.py
 def extract_upper(phrase):
+    """
+    extract_upper takes a string and returns a list containing
+    only the uppercase characters from the string
+
+    >>> extract_upper("Hello There, BOB")
+    ['H', 'T', 'B', 'O', 'B']
+    """
     return list(filter(str.isupper, phrase))
 
 def extract_lower(phrase):
--- using_modules/main.py
+++ using_modules/main.py
@@ -1,7 +1,8 @@
 # python -m using_modules.main
 from helpers.strings import extract_lower, extract_upper
 from helpers import variables
+import helpers
 
 print(f"Lowercase letters (from strings): {extract_lower(variables.name)}")
 print(f"Uppercase letters (from package): {extract_upper(variables.name)}")
-print(f"Off of helpers: {extract_lower(variables.name)}")
+print(f"Off of helpers: {helpers.strings.extract_lower(variables.name)}")
