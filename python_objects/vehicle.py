b890f442b648b9d1d93daafb4c4c1dfe8d94b020
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-23 23:32:52 +0800

    Custom Constructors, Class Methods, and Decorators

--- python_objects/vehicle.py
+++ python_objects/vehicle.py
@@ -2,14 +2,22 @@ class Vehicle:
     """
     Vehicle models a vehicle w/ tires and an engine
     """
+    default_tire = 'tire'
 
     def __init__(self, engine, tires):
         self.engine = engine
         self.tires = tires
         self.serial_number = ''
 
+    @classmethod
+    def bicycle(cls, tires=None):
+        if not tires:
+            tires = [cls.default_tire, cls.default_tire]
+        return cls(None, tires)
+
     def description(self):
         print(f"A vehicle with an {self.engine} engine, and {self.tires} tires")
+
 
 if __name__ == "__main__":
     civic = Vehicle('4-cylinder', ['front-driver', 'front-passenger', 'rear-driver', 'rear-passenger'])
@@ -23,3 +31,9 @@ if __name__ == "__main__":
     honda.serial_number = '1234'
     print(honda.serial_number)
     del honda.serial_number
+
+    bike = Vehicle.bicycle()
+    print(bike)
+    bike.description()
+    print(bike.engine)
+    print(bike.tires)
