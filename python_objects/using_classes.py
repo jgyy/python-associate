0f8060ddc45a81c41a6e272b0d4eaaf8d846a8f2
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-25 12:11:01 +0800

    Creating and Using Python Classes

---
+++ python_objects/using_classes.py
@@ -0,0 +1,29 @@
+# python -m python_objects.using_classes
+from python_objects.employee import Employee
+
+employee_1 = Employee(
+    name="Kevin Bacon",
+    title="Executive Producer",
+    email_address="kbacon@example.com",
+    phone_number="555-867-5309",
+)
+employee_2 = Employee("Bruce Wayne", "bwayne@example.com", "CEO")
+
+assert (
+    employee_1.email_signature(include_phone=True)
+    == "Kevin Bacon - Executive Producer\nkbacon@example.com (555-867-5309)"
+), f"Expected 'Kevin Bacon - Executive Producer\nkbacon@example.com (555-867-5309)' but got {employee_1.email_signature(include_phone=True)}"
+
+assert (
+    employee_2.email_signature() == "Bruce Wayne - CEO\nbwayne@example.com"
+), f"Expected 'Bruce Wayne - CEO\nbwayne@example.com' but got {employee_2.email_signature()}"
+
+assert (
+    employee_2.email_signature(include_phone=True)
+    == "Bruce Wayne - CEO\nbwayne@example.com"
+), f"Expected 'Bruce Wayne - CEO\nbwayne@example.com' but got {employee_2.email_signature(include_phone=True)}"
+
+print(employee_1.email_signature(include_phone=True))
+print(employee_2.email_signature())
+print(employee_2.email_signature(include_phone=True))
+
