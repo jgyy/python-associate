8c828c9ba829e3e519f9b1f69e3a524c6e998e24
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-27 13:41:03 +0800

    Creating Custom Exception Types

--- exception_handling/using_exceptions.py
+++ exception_handling/using_exceptions.py
@@ -1,10 +1,11 @@
+# python -m exception_handling.using_exceptions
 import sys
 
+from exception_handling.cli import main
+from exception_handling.cli.errors import ArgumentError
+
 try:
-    if len(sys.argv) < 2:
-        raise IndexError('Not enough arguments')
-except IndexError as err:
-    print("IndexError: " + str(err))
-else:
-    name = sys.argv[1]
-    print(f"Name is {name}")
+    main()
+except ArgumentError as err:
+    print(f"Error: {err}")
+    sys.exit(1)
